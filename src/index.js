027e053764a693c580d17fad7fecc51dcda98b2c
BondarenkoAlla <bondarenkoalla961@gmail.com>
2021-05-11 21:05:36 +0300

    HW10

--- src/index.js
+++ src/index.js
@@ -15,30 +15,30 @@ const Theme = {
 };
 
 const checkbox = document.querySelector('.theme-switch__toggle');
-const body = document.querySelector('body');
+const body = document.body;
 
 checkbox.addEventListener('change', changeTheme);
 
-if (!localStorage.theme) {
+if (localStorage.getItem('theme') === null) {
   localStorage.setItem('theme', Theme.LIGHT);
 }
-body.classList = localStorage.getItem('theme');
+body.className = localStorage.getItem('theme');
 if (localStorage.getItem('theme') === Theme.DARK) {
   checkbox.checked = true;
 }
 
 function changeTheme() {
-  if (checkbox.checked === true) {
-    if (body.className = Theme.LIGHT) {
+  if (checkbox.checked) {
+    if (body.className === Theme.LIGHT) {
       body.classList.remove(Theme.LIGHT);
     }
     body.classList.add(Theme.DARK);
-    checkbox.checked = true;
+    
       
-  } else if ((body.className = Theme.DARK)) {
+  } else  {
     body.classList.remove(Theme.DARK);
     body.classList.add(Theme.LIGHT);
-    checkbox.checked = false;
+    
   }
   localStorage.setItem('theme', body.classList);
 }
